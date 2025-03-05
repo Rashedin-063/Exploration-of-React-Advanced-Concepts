d322320083f92900f4d1468ffd225954f442d994
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-05 11:58:06 +0600

    updated

--- src/Concepts/LWS-useRef.jsx
+++ src/Concepts/LWS-useRef.jsx
@@ -1,6 +1,57 @@
+import { useRef } from "react";
+
 const LWSUseRef = () => {
+
+  const emailRef = useRef();
+    const passwordRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>LWSUseRef</div>
-  )
-}
-export default LWSUseRef
+    <div>
+      <h3 className='text-3xl lg:text-4xl font-semibold mb-8 text-center'>
+        Exploration of useRef - LWS
+      </h3>
+      <form
+        onSubmit={handleSubmit}
+        className='border border-blue-950 rounded-md w-[400px] lg:w-[500px] p-8'
+      >
+        <div className='flex flex-col space-y-1 my-4'>
+          <label htmlFor='email' className='text-lg leading-relaxed'>
+            Email
+          </label>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            placeholder='Enter your email'
+            className='py-2 rounded-lg pl-2'
+            ref={emailRef}
+          />
+        </div>
+        <div className='flex flex-col space-y-1'>
+          <label htmlFor='password' className='text-lg leading-relaxed'>
+            Password
+          </label>
+          <input
+            type='password'
+            name='password'
+            id='password'
+            placeholder='Password'
+            className='py-2 rounded-lg pl-2'
+            ref={passwordRef}
+          />
+        </div>
+        <button
+          type='submit'
+          className='mt-6 bg-blue-700 w-full py-2 rounded-lg text-white hover:bg-blue-800'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+export default LWSUseRef;
